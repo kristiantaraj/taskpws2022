c1f5c74a77c207d10b03032f2107dd1fc9ee139d
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-10-12 15:51:24 +0200

    20221012

--- server.js
+++ server.js
@@ -9,8 +9,22 @@ const config = {
 // express initialization
 const app = express()
 
+let data = {
+    firstName: 'Mariusz',
+    lastName: 'Jarocki',
+    yearOfBirth: 1969
+}
+
+const api = (req, res) => {
+    console.log('api called', req.method, req.url)
+    res.json(data)
+}
+
 // source of static content
 app.use(express.static('frontend'))
+
+// special endpoint for data
+app.all('/api', api)
 
 // main process
 app.listen(config.port)
