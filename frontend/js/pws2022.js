f24637c63120857b4c213b09d345649f64e4cf9b
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-10-20 09:42:07 +0200

    ng-repeat

--- frontend/js/pws2022.js
+++ frontend/js/pws2022.js
@@ -1,12 +1,21 @@
 const app = angular.module('pws2022', [])
 
-app.controller('MainCtrl', [ function() {
+app.controller('MainCtrl', [ '$http', function($http) {
     console.log('MainCtrl controller started')
     let ctrl = this
+
+    ctrl.persons = []
 
     ctrl.person = {
         firstName: 'John',
         lastName: 'Doe',
         yearOfBirth: 1999
     } 
+
+    $http.get('/api').then(
+        function(res) {
+            ctrl.persons = res.data
+        },
+        function(err) {}
+    )
 }])
