43ef96c966103a92b814682bdff9d40ff6cbcb46
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-10-20 08:57:42 +0200

    Data bound to view

--- frontend/js/pws2022.js
+++ frontend/js/pws2022.js
@@ -2,4 +2,11 @@ const app = angular.module('pws2022', [])
 
 app.controller('MainCtrl', [ function() {
     console.log('MainCtrl controller started')
+    let ctrl = this
+
+    ctrl.person = {
+        firstName: 'John',
+        lastName: 'Doe',
+        yearOfBirth: 1999
+    } 
 }])
