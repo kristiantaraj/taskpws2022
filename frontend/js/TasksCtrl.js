fcfe5f679fba856b96bc2e0956e94d195894f3ea
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-12-21 15:40:51 +0100

    The simplest presentation of the tasks

--- frontend/js/TasksCtrl.js
+++ frontend/js/TasksCtrl.js
@@ -3,4 +3,13 @@ app.controller('TasksCtrl', [ '$http', 'common', function($http, common) {
     let ctrl = this
 
     const endpoint = '/api/tasks'
+
+    ctrl.tasks = []
+    
+    $http.get(endpoint).then(
+        function(res) {
+            ctrl.tasks = res.data.records
+        },
+        function(err) { common.alert('Cannot retrieve tasks', 'danger') }
+    )
 }])
