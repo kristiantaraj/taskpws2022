e4ac40f3c2f69fb3a028d0c450ff52bfa68e3581
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-12-21 16:02:58 +0100

    Selection of the project (not finished)

--- frontend/js/TasksCtrl.js
+++ frontend/js/TasksCtrl.js
@@ -5,11 +5,24 @@ app.controller('TasksCtrl', [ '$http', 'common', function($http, common) {
     const endpoint = '/api/tasks'
 
     ctrl.tasks = []
-    
+    ctrl.projects = []
+    ctrl.selectedProjectId = null
+
+    ctrl.test = function() {
+        console.log(ctrl.selectedProjectId)
+    }
+
     $http.get(endpoint).then(
         function(res) {
             ctrl.tasks = res.data.records
         },
         function(err) { common.alert('Cannot retrieve tasks', 'danger') }
+    )
+
+    $http.get('/api/projects').then(
+        function(res) {
+            ctrl.projects = res.data.records
+        },
+        function(err) { common.alert('Cannot retrieve projects', 'danger') }
     )
 }])
