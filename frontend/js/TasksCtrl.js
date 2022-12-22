668aa9fd949cd7b34dbafbcc33ea41f8dbe0869e
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2022-12-22 18:19:04 +0100

    Selecting project and loading its tasks

--- frontend/js/TasksCtrl.js
+++ frontend/js/TasksCtrl.js
@@ -8,20 +8,20 @@ app.controller('TasksCtrl', [ '$http', 'common', function($http, common) {
     ctrl.projects = []
     ctrl.selectedProjectId = null
 
-    ctrl.test = function() {
-        console.log(ctrl.selectedProjectId)
+    ctrl.loadTasks = function() {
+        $http.get(endpoint + '?filter=' + ctrl.selectedProjectId).then(
+            function(res) {
+                ctrl.tasks = res.data.records
+            },
+            function(err) { common.alert('Cannot retrieve tasks', 'danger') }
+        )            
     }
-
-    $http.get(endpoint).then(
-        function(res) {
-            ctrl.tasks = res.data.records
-        },
-        function(err) { common.alert('Cannot retrieve tasks', 'danger') }
-    )
 
     $http.get('/api/projects').then(
         function(res) {
             ctrl.projects = res.data.records
+            ctrl.selectedProjectId = ctrl.projects.length > 0 ? ctrl.projects[0]._id : null
+            ctrl.loadTasks()
         },
         function(err) { common.alert('Cannot retrieve projects', 'danger') }
     )
